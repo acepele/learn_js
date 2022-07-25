a978e9370aac28185426accfbbeb6b070832a755
acepele <47071340+acepele@users.noreply.github.com>
2022-07-24 22:54:23 -0500

    Add files via upload

---
+++ learn-json/scripts/create.js
@@ -0,0 +1,9 @@
+function addme() {
+    const person = {};
+    person.firstName = "Aljeda";
+    person.lastName = "Cepele";
+    person.email = "aljedacepele93@gmail.com";
+    person.phone = 815 - 261 - 7121;
+    const person = JSON.stringify(person);
+    document.getElementById("js-value").innerHTML = myJSON;
+}
